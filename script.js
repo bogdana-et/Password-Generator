79c1936225a1d82605015ff56d755518e9e81928
Bogdana <bogdanatuhikyan@outlook.com>
2023-12-15 18:24:15 +0000

    Added notes to js file

--- script.js
+++ script.js
@@ -88,19 +88,53 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+const charOptions = [];
+//Can be empty string or empty array
+const generatedPassword = "";
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+   // Prompt for password length
+  //At least 8 characters, no more than 128
+  //Conditional to check that the number that was entered is in range
+  //Prompts store data as strings, s oneed to parse into a number
+  //If the user's input is out of range, either return out of the function or call the function again
+
+  //Confirm which character sets to use
+  //If the user answers false for all, either return out of the function or call the function again
+ //Generate a random character for each selected character set
+  //Either push selected character sets to a mega-array of all selected characters
+  //OR you can keep the arrays separate and generate a random number to select the array and another to select the index
+
+  //Once character sets are selected, move on to generating random characters
 
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  // Need a variable to hold the password as it's being generated
+  //Need  avariable to hold the index that's being generated
+  //Generate a random number
+  //That number is the index for a character in the mega-array
+  //So then, mega-array[generated-index] is the actual character
+  //Add that character to the password
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  // Generate a password when the button is clicked
+  // Present a series of prompts for password criteria
+    // Length of password
+      // At least 8 characters but no more than 128.
+    // Character types
+      // Lowercase
+      // Uppercase
+      // Numeric
+      // Special characters ($@%&*, etc)
+  // Code should validate for each input and at least one character type should be selected
+  // Once prompts are answered then the password should be generated and displayed in an alert or written to the page
 
+ 
 }
 
 // Get references to the #generate element
